57df2249aaf8d619303fd8c29f72408c10eacbae
WonXd <103139749+WonXd@users.noreply.github.com>
2022-06-08 17:41:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6287892711054', '6287892711054', '6287892711054'] // Masukan nomot kalian
-global.mods = ['6287892711054', '6287892711054'] // Moderator
+global.owner = ['62895337447795', '62895337447795', '62895337447795'] // Masukan nomot kalian
+global.mods = ['62895337447795', '62895337447795'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -54,8 +54,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'hehe'
-  var sticker_author = 'by ShiroBotz'
+  var sticker_name = 'WONXD-BOT'
+  var sticker_author = '@sampeanikupicek'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
